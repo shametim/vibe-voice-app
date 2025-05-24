a0a5b27aad2765f2c517eca1d25ca84f0b80364a
Your Name <9932250+shametim@users.noreply.github.com>
2025-05-24 23:39:53 +0000

    hi

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
-<h1 className="text-2xl font-bold font-franklin-gothic-medium p-4 text-blue-500 text-center">
-  Hey, my name is Ben
-</h1>
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold font-franklin-gothic-medium p-4 text-blue-500 text-center">
+        Hey, my name is Martin
+      </h1>
+    </div>
+  );
+}
