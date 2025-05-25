b81279072434c359f8712bf7ee35edeeea3a86e4
Your Name <9932250+shametim@users.noreply.github.com>
2025-05-25 00:06:11 +0000

    refactor: Remove header title

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,6 @@ import '../styles.css';
 export const App = () => {
   return (
     <div className="dark bg-gray-900 min-h-screen text-white flex flex-col items-center justify-center p-8">
-      <h1 className="text-3xl font-bold mb-4">Something New York Times</h1>
     </div>
   );
 };
